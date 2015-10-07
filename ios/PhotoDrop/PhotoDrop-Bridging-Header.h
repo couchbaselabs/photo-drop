1a1dfe041d4dbb2f3caa7c16553d15ae1bddf646
James Nocentini <james.nocentini@gmail.com>
2015-10-07 12:41:43 +0100

    use IP for hostname on iOS

--- ios/PhotoDrop/PhotoDrop-Bridging-Header.h
+++ ios/PhotoDrop/PhotoDrop-Bridging-Header.h
@@ -4,3 +4,6 @@
 
 #import <CouchbaseLite/CouchbaseLite.h>
 #import <CouchbaseLiteListener/CBLListener.h>
+
+// Required to use the IP address as the hostname (for replication with PhotoDrop Android)
+#include <ifaddrs.h>
--- ios/PhotoDrop/ReceiveViewController.swift
+++ ios/PhotoDrop/ReceiveViewController.swift
@@ -119,6 +119,39 @@ class ReceiveViewController: UIViewController, UICollectionViewDataSource, UICol
         }
     }
 
+    func getIFAddresses() -> [String] {
+        var addresses = [String]()
+
+        // Get list of all interfaces on the local machine:
+        var ifaddr : UnsafeMutablePointer<ifaddrs> = nil
+        if getifaddrs(&ifaddr) == 0 {
+
+            // For each interface ...
+            for (var ptr = ifaddr; ptr != nil; ptr = ptr.memory.ifa_next) {
+                let flags = Int32(ptr.memory.ifa_flags)
+                var addr = ptr.memory.ifa_addr.memory
+
+                // Check for running IPv4, IPv6 interfaces. Skip the loopback interface.
+                if (flags & (IFF_UP|IFF_RUNNING|IFF_LOOPBACK)) == (IFF_UP|IFF_RUNNING) {
+                    if addr.sa_family == UInt8(AF_INET) || addr.sa_family == UInt8(AF_INET6) {
+
+                        // Convert interface address to a human readable string:
+                        var hostname = [CChar](count: Int(NI_MAXHOST), repeatedValue: 0)
+                        if (getnameinfo(&addr, socklen_t(addr.sa_len), &hostname, socklen_t(hostname.count),
+                            nil, socklen_t(0), NI_NUMERICHOST) == 0) {
+                                if let address = String.fromCString(hostname) {
+                                    addresses.append(address)
+                                }
+                        }
+                    }
+                }
+            }
+            freeifaddrs(ifaddr)
+        }
+
+        return addresses
+    }
+
     func stopListener() {
         if listener != nil {
             listener.stop()
@@ -140,6 +173,9 @@ class ReceiveViewController: UIViewController, UICollectionViewDataSource, UICol
             if let urlComp = NSURLComponents(string: url.absoluteString!) {
                 urlComp.user = username
                 urlComp.password = password
+
+                urlComp.host = getIFAddresses()[0]
+
                 return urlComp.URL
             }
         }
